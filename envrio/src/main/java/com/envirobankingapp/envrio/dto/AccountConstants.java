509f2e4a61266e6b3cbb195f979cef6277295a26
Tshegofatso <tshegofatsodingaan45@gmail.com>
2023-09-01 07:58:15 +0200

    account constants class

---
+++ envrio/src/main/java/com/envirobankingapp/envrio/dto/AccountConstants.java
@@ -0,0 +1,21 @@
+package com.envirobankingapp.envrio.dto;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "account-constants")
+public class AccountConstants {
+
+    @Value("${account-constants.minimum}")
+    public BigDecimal minimum;
+
+    @Value("${account-constants.overdraft}")
+    public BigDecimal overdraft;
+
+}
