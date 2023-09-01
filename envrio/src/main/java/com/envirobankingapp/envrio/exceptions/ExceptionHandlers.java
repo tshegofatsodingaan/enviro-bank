f8b021fd24559f8f1673986172369cf6084e7063
Tshegofatso <tshegofatsodingaan45@gmail.com>
2023-09-01 11:20:40 +0200

    Added exception handler class

---
+++ envrio/src/main/java/com/envirobankingapp/envrio/exceptions/ExceptionHandlers.java
@@ -0,0 +1,19 @@
+package com.envirobankingapp.envrio.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlers {
+    @ExceptionHandler(InsufficientFundsException.class)
+    public ResponseEntity<Object> message(InsufficientFundsException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> message(EntityNotFoundException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
