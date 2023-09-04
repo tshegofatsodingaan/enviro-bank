1db43c0176ea7b981cce636f958beaf628eba019
Tshegofatso <tshegofatsodingaan45@gmail.com>
2023-09-04 12:16:20 +0200

    modified the pom to match the new package naming. Changed the table name in the insert script. Changing the data types in the database to be decimals.

--- envrio/src/main/resources/db/migration/V1.01__create_users_table.sql
+++ envrio/src/main/resources/db/migration/V1.01__create_users_table.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS transaction (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     account_num SERIAL NOT NULL,
     type_of_transaction VARCHAR(50) NOT NULL,
-    transaction_amount VARCHAR(50) NOT NULL,
+    transaction_amount DECIMAL NOT NULL,
     active BOOLEAN DEFAULT false,
     account_id INTEGER REFERENCES account(id)
 );
