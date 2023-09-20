a645863e7235de4a9d11613c27761c8b06358200
Tshegofatso <tshegofatsodingaan45@gmail.com>
2023-09-20 09:02:18 +0200

    validated the RSA Id and the phone number

--- envrio/src/main/java/com/enviro/envirobankingapp/dto/CustomerDto.java
+++ envrio/src/main/java/com/enviro/envirobankingapp/dto/CustomerDto.java
@@ -10,9 +10,12 @@ public class CustomerDto {
 
     private String name;
     private String surname;
-    @Pattern(regexp = "\\d{13}",message = "Invalid phone number.")
+    @Pattern(regexp = "\\d{13}",message = "Invalid identification number.")
+    @Pattern(regexp = "(([0-9]{2})(0|1)([0-9])([0-3])([0-9]))([ ]?)(([0-9]{4})([ ]?)([0-1][8]([ ]?)[0-9]))",message = "Invalid identification number.")
     private String idNumber;
-    @Pattern(regexp = "\\d{10}",message = "Invalid phone number.")
+    @Pattern(regexp = "^0[0-9]{9}$",message = "Please enter valid phone number, without the country code.")
     private String phoneNumber;
     private String email;
 }
+
+// 0207290750083
