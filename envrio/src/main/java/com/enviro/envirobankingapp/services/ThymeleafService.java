dc055c534da5279030b79dbe656e98acff6e0ddc
Tshegofatso <tshegofatsodingaan45@gmail.com>
2023-09-27 12:12:15 +0200

    Migrated the email html to be using thymeleaf. added two files for such.

---
+++ envrio/src/main/java/com/enviro/envirobankingapp/services/ThymeleafService.java
@@ -0,0 +1,8 @@
+package com.enviro.envirobankingapp.services;
+
+import java.util.Map;
+
+public interface ThymeleafService {
+
+    String createContent(String template, Map<String, Object> variables);
+}
